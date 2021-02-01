5120b29f14edcc4330146607d43d8acfc9df3e20
Vinicius <vinicius.raymundo@zup.com.br>
2021-02-01 15:42:35 -0300

    Adicionar menu de interação com usuario

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -3,6 +3,7 @@ package com.br.zup;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,6 +11,12 @@ public class Main {
         // Programa que cadastra cada animal de cada zoologico de cada cidade.
 
         Map<String, List<String>> dicionarioAnimais = new HashMap<>();
+
+        boolean continuar = true;
+        while (continuar){
+            System.out.println("Digite: \n1 para cadastrar");
+            int resposta = new Scanner(System.in).nextInt();
+        }
 
     }
 }
