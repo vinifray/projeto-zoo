8451f6881e8a3c1d3be6bff15cc1b23afd4c4269
Vinicius <vinicius.raymundo@zup.com.br>
2021-02-01 16:00:01 -0300

    Adicionar função de cadastro de zoologico com animais

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -1,9 +1,6 @@
 package com.br.zup;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -16,6 +13,23 @@ public class Main {
         while (continuar){
             System.out.println("Digite: \n1 para cadastrar");
             int resposta = new Scanner(System.in).nextInt();
+
+            if(resposta == 1){
+                System.out.println("Qual zoologico vc vai cadastrar? ");
+                String zoologico = new Scanner(System.in).nextLine();
+                List<String> animais = new ArrayList<>();
+                String retorno = "";
+                do{
+                    System.out.println("Digite o nome do animal para cadastrar ou sair para finalizar");
+                    retorno = new Scanner(System.in).nextLine();
+                    if(!retorno.equalsIgnoreCase("sair")){
+                        animais.add(retorno);
+                    }
+                }while (!retorno.equalsIgnoreCase("sair"));
+
+                dicionarioAnimais.put(zoologico, animais);
+            }
+
         }
 
     }
