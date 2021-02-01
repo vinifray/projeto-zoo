3397a7a936877ccb8d5f55c943a12f4ce9f6db50
Vinicius <vinicius.raymundo@zup.com.br>
2021-02-01 16:03:03 -0300

    Adicionar função para criar scanner

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class Main {
 
+    public static Scanner criarScanner(){
+        return new Scanner(System.in);
+    }
+
     public static void main(String[] args) {
         // Programa que cadastra cada animal de cada zoologico de cada cidade.
 
