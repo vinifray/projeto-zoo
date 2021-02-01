cccb1ce671107137ad5ecee64386ab2d7c87e504
Vinicius <vinicius.raymundo@zup.com.br>
2021-02-01 15:39:37 -0300

    Adicionar dicionario de animais de cada zoologico

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -1,11 +1,15 @@
 package com.br.zup;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
         // Programa que cadastra cada animal de cada zoologico de cada cidade.
 
-
+        Map<String, List<String>> dicionarioAnimais = new HashMap<>();
 
     }
 }
